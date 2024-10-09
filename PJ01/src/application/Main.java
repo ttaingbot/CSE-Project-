50e5a01fa46c862bc25e69a8610cfa70855219b4
arnav <arnav@10.127.29.57>
2024-10-09 02:06:00 -0700

    minor changes and trying to get push to work

--- PJ01/src/application/Main.java
+++ PJ01/src/application/Main.java
@@ -84,7 +84,7 @@ public class Main {
 	}
 	
 	// the goal of this function is to check that the inputted line is a single char of a type allowed
-	// returns a boolean if the char follows all of the required conditions
+	// returns a boolean if the char follows all of required conditions
 	public static boolean checkChar(Scanner scanner, String allowed, String input) {
 		
 		
